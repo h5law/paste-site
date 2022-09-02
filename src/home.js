7490fa124ae9f96c0492ecc7a0dc9b5531c9d7b3
Harry Law <53987565+h5law@users.noreply.github.com>
2022-09-02 17:41:30 +0100

    Add create paste button and textarea visual elements

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 
 const mapLangName = new Map([
     ["bash", "Bash"],
@@ -76,16 +76,77 @@ function ExpiresIn(props) {
     );
 }
 
+function CreateButton(props) {
+    function handleSubmit(e) {
+        e.preventDefault()
+        props.cp()
+    }
+
+    return (
+        <div id="create-container">
+            <button id="create-button" onSubmit={handleSubmit}>
+                Create Paste
+            </button>
+        </div>
+    );
+}
+
+function PasteCode(props) {
+    const [screenSize, getDimension] = useState({
+        dWidth:     document.body.offsetWidth,
+        dHeight:    document.documentElement.clientHeight,
+    });
+    const setDimension = () => {
+        getDimension({
+            dWidth:     document.body.offsetWidth,
+            dHeight:    document.documentElement.clientHeight,
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', setDimension);
+
+        return (() => {
+            window.removeEventListener('resize', setDimension);
+        });
+    }, [screenSize]);
+
+    const textAreaStyle = {
+        maxWidth: `${screenSize.dWidth - 10}px`,
+        maxHeight: `${screenSize.dHeight}px`,
+    };
+
+    function handleChange(e) {
+        e.preventDefault();
+        props.sc(e.target.value.split('\n'));
+    }
+
+    const languageStr = `language-${props.ft}`;
+
+    return (
+        <pre id="code-container">
+            <code class={languageStr}>
+                <textarea id="code" name="paste-code" autofocus
+                          style={textAreaStyle} rows="30" cols="100"
+                          onChange={handleChange}>
+                </textarea>
+            </code>
+        </pre>
+    );
+}
+
 export default function Home() {
     const [filetype, setFiletype] = useState("plaintext");
     const [expiresIn, setExpiresIn] = useState(14);
+    const [content, setContent] = useState([]);
 
     return (
         <div id="container">
             <FtSelector sft={setFiletype} />
-            <p>{filetype}</p>
             <ExpiresIn ei={expiresIn} sei={setExpiresIn} />
             <p>{expiresIn} day{expiresIn > 1 ? "s" : ""}</p>
+            <CreateButton />
+            <PasteCode ft={filetype} sc={setContent} />
         </div>
     );
 }
