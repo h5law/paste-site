7f7b9f9d47931a5f3c712ca28db60da0aa11fdbe
Harry Law <53987565+h5law@users.noreply.github.com>
2022-09-05 00:15:47 +0100

    Add AsyncRoutes to load only the necissary content for each page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import Router from 'preact-router';
 import { h } from 'preact';
+import AsyncRoute from 'preact-async-route';
 
-import Home from './home.js';
-import Paste from './paste.js';
 import './style';
+
+function getHome(url, cb, props) {
+    const componentOrPromise = import('./home.js');
+    if (componentOrPromise.then) {
+        return componentOrPromise.then(module => module.default);
+    } else if (componentOrPromise.default) {
+        cb({ component: componentOrPromise });
+    }
+}
+
+function getPaste(url, cb, props) {
+    const componentOrPromise = import('./paste.js');
+    if (componentOrPromise.then) {
+        return componentOrPromise.then(module => module.default);
+    } else if (componentOrPromise.default) {
+        cb({ component: componentOrPromise });
+    }
+}
 
 export default function App() {
     return (
         <div id="app">
             <Router>
-                <Paste path="/:uuid" />
-                <Home default />
+                <AsyncRoute path="/:uuid" getComponent={getPaste} />
+                <AsyncRoute default getComponent={getHome} />
             </Router>
         </div>
     );
