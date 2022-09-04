3957e8463df4b96b020a4712590c0e5d41a98201
Harry Law <53987565+h5law@users.noreply.github.com>
2022-09-05 00:28:17 +0100

    Remove asyncroutes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 import Router from 'preact-router';
 import { h } from 'preact';
-import AsyncRoute from 'preact-async-route';
 
+import Home from './routes/home.js';
+import Paste from './routes/paste.js';
 import './style';
-
-function getHome(url, cb, props) {
-    const componentOrPromise = import('./home.js');
-    if (componentOrPromise.then) {
-        return componentOrPromise.then(module => module.default);
-    } else if (componentOrPromise.default) {
-        cb({ component: componentOrPromise });
-    }
-}
-
-function getPaste(url, cb, props) {
-    const componentOrPromise = import('./paste.js');
-    if (componentOrPromise.then) {
-        return componentOrPromise.then(module => module.default);
-    } else if (componentOrPromise.default) {
-        cb({ component: componentOrPromise });
-    }
-}
 
 export default function App() {
     return (
         <div id="app">
             <Router>
-                <AsyncRoute path="/:uuid" getComponent={getPaste} />
-                <AsyncRoute default getComponent={getHome} />
+                <Paste path="/:uuid" />
+                <Home default />
             </Router>
         </div>
     );
